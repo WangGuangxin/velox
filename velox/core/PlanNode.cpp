f6202b08016df77afeb2a203f41432b840b59a58
wangguangxin.cn <wangguangxin.cn@bytedance.com>
2024-05-23 08:27:16 +0800

    support explode outer

--- velox/core/PlanNode.cpp
+++ velox/core/PlanNode.cpp
@@ -1056,12 +1056,14 @@ UnnestNode::UnnestNode(
     std::vector<FieldAccessTypedExprPtr> unnestVariables,
     const std::vector<std::string>& unnestNames,
     const std::optional<std::string>& ordinalityName,
-    const PlanNodePtr& source)
+    const PlanNodePtr& source,
+    const bool isOuter)
     : PlanNode(id),
       replicateVariables_{std::move(replicateVariables)},
       unnestVariables_{std::move(unnestVariables)},
       withOrdinality_{ordinalityName.has_value()},
-      sources_{source} {
+      sources_{source},
+      isOuter_{isOuter} {
   // Calculate output type. First come "replicate" columns, followed by
   // "unnest" columns, followed by an optional ordinality column.
   std::vector<std::string> names;
@@ -1122,6 +1124,7 @@ folly::dynamic UnnestNode::serialize() const {
   if (withOrdinality_) {
     obj["ordinalityName"] = outputType()->names().back();
   }
+  obj["isOuter"] = isOuter_;
   return obj;
 }
 
@@ -1136,6 +1139,7 @@ PlanNodePtr UnnestNode::create(const folly::dynamic& obj, void* context) {
   if (obj.count("ordinalityName")) {
     ordinalityName = obj["ordinalityName"].asString();
   }
+  bool isOuter = obj["isOuter"].asBool();
 
   return std::make_shared<UnnestNode>(
       deserializePlanNodeId(obj),
@@ -1143,7 +1147,8 @@ PlanNodePtr UnnestNode::create(const folly::dynamic& obj, void* context) {
       std::move(unnestVariables),
       std::move(unnestNames),
       ordinalityName,
-      std::move(source));
+      std::move(source),
+      isOuter);
 }
 
 AbstractJoinNode::AbstractJoinNode(
