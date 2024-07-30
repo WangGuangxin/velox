382837e14ec6bdf332cf95943f6acd1c935ec6bc
wangguangxin.cn <wangguangxin.cn@bytedance.com>
2024-07-30 20:50:42 +0800

    support sparksql map_from_array

--- velox/functions/sparksql/Register.cpp
+++ velox/functions/sparksql/Register.cpp
@@ -114,9 +114,7 @@ static void workAroundRegistrationMacro(const std::string& prefix) {
 
   // This is the semantics of spark.sql.ansi.enabled = false.
   registerElementAtFunction(prefix + "element_at", true);
-
-  VELOX_REGISTER_VECTOR_FUNCTION(
-      udf_map_allow_duplicates, prefix + "map_from_arrays");
+ 
   VELOX_REGISTER_VECTOR_FUNCTION(
       udf_concat_row, exec::RowConstructorCallToSpecialForm::kRowConstructor);
   // String functions.
